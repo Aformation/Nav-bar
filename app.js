7fb3d7c5c79eb358cdfb296bf204c0b593cbb760
Aformation <59423821+Aformation@users.noreply.github.com>
2020-10-06 15:17:45 +0200

    v0.1

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 // classList - shows/gets all classes
+
 // contains - checks classList for specific class
+
 // add - add class
+
 // remove - remove class
+
 // toggle - toggles class
+const navToggle = document.getElementsByClassName('nav-toggle');
+const links = document.getElementsByClassName('links');
+
+navToggle.addEventListener('click', function(){
+    console.log(links.classList);
+});
